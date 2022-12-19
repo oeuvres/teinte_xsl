fa74ad7c758eef2452397165cbfc517a677d8287
Frédéric Glorieux <frederic.glorieux@fictif.org>
2022-12-19 21:59:46 +0100

    A trick to get a path by Class path

---
+++ Xsl.php
@@ -0,0 +1,24 @@
+<?php declare(strict_types=1);
+
+/**
+ * Part of Teinte https://github.com/oeuvres/teinte_php
+ * Copyright (c) 2020 frederic.glorieux@fictif.org
+ * Copyright (c) 2013 frederic.glorieux@fictif.org & LABEX OBVIL
+ * Copyright (c) 2012 frederic.glorieux@fictif.org
+ * BSD-3-Clause https://opensource.org/licenses/BSD-3-Clause
+ */
+namespace Oeuvres\Teinte;
+
+class Xsl
+{
+    /** Where it’s place here */
+    static protected string $dir;
+    /**
+     * Initialize static variable
+     */
+    static public function init()
+    {
+        self::$dir = __DIR__ . DIRECTORY_SEPARATOR;
+    }
+}
+Xsl::init();
